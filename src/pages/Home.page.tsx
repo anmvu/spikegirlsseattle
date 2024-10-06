4efc123afb48d35a01381eebc8a2f3d61876a810
An <anvu94@gmail.com>
2024-10-06 02:55:05 -0700

    test

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,13 +1,11 @@
 import { Instagram } from '@/components/Instagram/Instagram';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
-import { Facebook } from '@/components/Facebook/Facebook';
 
 export function HomePage() {
   return (
     <>
       <Welcome />
-      <ColorSchemeToggle />
+      {/* <ColorSchemeToggle /> */}
       {/* <Instagram/> */}
       {/* <Facebook/> */}
     </>
